39fe50368a1e7c43c396dea82b5c456f0cf38394
Samuel Courville <swcourville@gmail.com>
2024-04-01 23:12:36 -0700

    Delete rock/Rcrust/code directory

--- rock/Rcrust/code/run.r
+++
@@ -1,5 +0,0 @@
-setwd("D:/Rcrust/code")
-  manual_load<<-function(working_file,projects_directory=paste0(substring(getwd(),1,nchar(getwd())-4),"Projects")){
-    source(paste0(projects_directory,"/",working_file,"/Inputs/",working_file,".txt"))
-    source("main.r")
-  }
